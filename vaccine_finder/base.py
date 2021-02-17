624d9871009e13c1ea3095b0a4aff91166fda3b3
Natasha Singh <dukedesi22@gmail.com>
2021-02-17 16:54:34 -0500

    :building_construction: Factor out common finder functions, refactor riteaid

---
+++ vaccine_finder/base.py
@@ -0,0 +1,123 @@
+from abc import ABC, abstractmethod
+import os
+import json
+from pprint import pprint, pformat
+import logging
+import requests
+
+from vaccine_finder.utils import setup_logger, send_request
+from vaccine_finder.notify import Notifier
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+INPUT_FILE = os.path.abspath(os.path.join(ROOT_DIR, "inputs.json"))
+DEFAULT_PHONE_NUM = "+14846206937"
+
+
+class BaseAppointmentFinder(ABC):
+    def __init__(
+        self,
+        store_label,
+        scheduler_endpoint,
+        debug=False,
+        input_file=INPUT_FILE,
+        cookie=None,
+    ):
+        setup_logger()
+        self.logger = logging.getLogger(type(self).__name__)
+        self.store_label = store_label
+        self.scheduler_endpoint = scheduler_endpoint
+        self.debug = debug
+
+        self.logger.info("Initializing {type(self).__name__} ...")
+        self.logger.info(f'DEBUG: {self.debug}')
+
+        # Create session with cookie
+        self.session = requests.Session()
+        if cookie:
+            self.session.cookies = requests.cookies.cookiejar_from_dict(cookie)
+
+        # Read inputs - zip_code, radius, subscribers to notify
+        self.zip_code = 19403
+        self.radius = 50
+        self.subscribers = []
+        if os.path.exists(input_file):
+            with open(input_file) as json_file:
+                inputs = json.load(json_file)
+                self.zip_code = inputs["location"]["zip_code"]
+                self.radius = inputs["location"]["radius"]
+                self.subscribers = inputs["subscribers"]
+                if self.debug:
+                    self.subscribers = {
+                        DEFAULT_PHONE_NUM: self.subscribers.get(
+                            DEFAULT_PHONE_NUM
+                        )
+                    }
+
+        self.notifier = Notifier()
+
+    def find(self, *args, **kwargs):
+        """
+        See _find
+        """
+        success = True
+        try:
+            notify = kwargs.pop('notify', False)
+            self.logger.info(f"NOTIFY: {notify}")
+            success = self._find(*args, **kwargs)
+            if success:
+                self.notify(send_notifications=notify)
+
+        except Exception as e:
+            success = False
+            self.logger.error("Something went wrong in vaccine finder!")
+            self.logger.exception(str(e))
+
+        if success:
+            self.logger.info("✅ At least one store has appointments!")
+        else:
+            self.logger.info("🚫 No stores have appointments!")
+
+        return success
+
+    def notify(self, send_notifications=False):
+        """
+        Notify users (text, email) that appointments are available in stores
+        """
+        self.logger.info("Notifying users of open appointments ...")
+        messages = [
+            "--------------------",
+            f"🚑 {self.store_label} Stores with Open Appointments!!!",
+        ]
+        custom_message = self._notification_message()
+        messages.append(custom_message)
+        messages.append(f"➡ To register, go to {self.scheduler_endpoint}")
+        output = "\n\n".join(messages)
+        if send_notifications:
+            self.notifier.send_texts(
+                output, phone_numbers=[
+                    ph for ph, email in self.subscribers.items()
+                ]
+            )
+            self.logger.info(
+                f"Sent texts to subscribers: {pformat(self.subscribers)}"
+            )
+        self.logger.info(output)
+
+    @abstractmethod
+    def _find(self, *args, **kwargs):
+        """
+        Called by find method. Must return boolean indicating success or 
+        not 
+
+        MUST BE IMPLEMENTED BY SUBCLASSES
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _notification_message(self):
+        """
+        Return the message to put in the success notification sent to users
+
+        MUST BE IMPLEMENTED BY SUBCLASSES
+        """
+        raise NotImplementedError
--- vaccine_finder/riteaid/finder.py
+++ vaccine_finder/riteaid/finder.py
@@ -6,6 +6,7 @@ import requests
 
 from vaccine_finder.utils import setup_logger, send_request
 from vaccine_finder.notify import Notifier
+from vaccine_finder.base import BaseAppointmentFinder
 
 CHECK_SLOTS_ENDPOINT = (
     "https://www.riteaid.com/services/ext/v2/vaccine/checkSlots"
@@ -13,83 +14,41 @@ CHECK_SLOTS_ENDPOINT = (
 GET_STORES_ENDPOINT = "https://www.riteaid.com/services/ext/v2/stores/getStores"
 SCHEDULER_ENDPOINT = "https://www.riteaid.com/pharmacy/apt-scheduler"
 VACCINE_DOSE_IDS = ["1"]
+STORE_LABEL = "RiteAid"
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 INPUT_FILE = os.path.abspath(os.path.join(ROOT_DIR, "inputs.json"))
 
 
-class RiteAidAppointmentFinder(object):
+class RiteAidAppointmentFinder(BaseAppointmentFinder):
     def __init__(
         self,
         debug=False,
         input_file=INPUT_FILE,
         cookie=None,
     ):
-        self.logger = setup_logger()
-        self.debug = debug
+        super().__init__(
+            STORE_LABEL, SCHEDULER_ENDPOINT,
+            debug=debug, input_file=input_file, cookie=cookie
+        )
 
-        self.logger.info("Initializing RiteAid Vaccine Finder ...")
-        self.logger.info(f'DEBUG: {self.debug}')
-        # Create session with cookie
-        self.session = requests.Session()
-        if cookie:
-            self.session.cookies = requests.cookies.cookiejar_from_dict(cookie)
-
-        # Read inputs - zip_code, radius, subscribers to notify
-        self.zip_code = 19403
-        self.radius = 50
-        self.subscribers = []
-        if os.path.exists(input_file):
-            with open(input_file) as json_file:
-                inputs = json.load(json_file)
-                self.zip_code = inputs["location"]["zip_code"]
-                self.radius = inputs["location"]["radius"]
-                self.subscribers = [
-                    (k, v) for k, v in inputs["subscribers"].items()
-                ]
-                if self.debug:
-                    self.subscribers = self.subscribers[0:1]
-
-        self.notifier = Notifier()
-
-    def find(self, *args, **kwargs):
-        """
-        See _find
-        """
-        success = True
-        try:
-            success = self._find(*args, **kwargs)
-        except Exception as e:
-            success = False
-            self.logger.error("Something went wrong in vaccine finder!")
-            self.logger.exception(str(e))
-
-        if success:
-            self.logger.info("✅ At least one store has appointments!")
-        else:
-            self.logger.info("🚫 No stores have appointments!")
-
-        return success
-
-    def _find(self, zip_code=None, radius=None, notify=True):
+    def _find(self, zip_code=None, radius=None):
         """
         Entrypoint for find script
 
         Find RiteAids where there are available covid vaccine appointments
         """
-        success = False
         zip_code = zip_code or self.zip_code
         radius = radius or self.radius
 
         self.logger.info("Starting vaccine finder ...")
-        self.logger.info(f'NOTIFY: {notify}')
 
         # Get list of stores to query
-        stores = self.get_stores(zip_code, radius)
+        stores = self._get_stores(zip_code, radius)
         if self.debug:
             stores = stores[0:2]
 
-        stores_with_appts = []
+        self.stores_with_appts = []
         for store in stores:
             store["fullAddress"] = (
                 f'{store["address"]} {store["city"]}, {store["state"]} '
@@ -116,18 +75,25 @@ class RiteAidAppointmentFinder(object):
             self.logger.info(f"Received response:\n{pformat(content)}")
 
             # Check availability - for vaccine dose 1/2
-            avail = self.check_availability(content, store)
+            avail = self._check_availability(content, store)
             if avail:
-                stores_with_appts.append(store)
-            success = success or avail
+                self.stores_with_appts.append(store)
 
-        # Notify people with stores that have appointments
-        if stores_with_appts:
-            self.notify(stores_with_appts, send_notifications=notify)
+        return len(self.stores_with_appts) > 0
 
-        return success
+    def _notification_message(self):
+        """
+        Create notification message to notify users (text, email) 
+        that appointments are available in stores
+        """
+        return "\n\n".join(
+            [
+                f"Store #{store['storeNumber']} at " f"{store['fullAddress']}"
+                for store in self.stores_with_appts
+            ]
+        )
 
-    def check_availability(self, content, store):
+    def _check_availability(self, content, store):
         """
         Check if either vaccine dose 1 or 2 have open appointments
         """
@@ -149,31 +115,7 @@ class RiteAidAppointmentFinder(object):
                 )
         return success
 
-    def notify(self, stores, send_notifications=False):
-        """
-        Notify users (text, email) that appointments are available in stores
-        """
-        self.logger.info("Notifying users of open appointments ...")
-        messages = [
-            "--------------------",
-            "🚑 RiteAid Stores with Open Appointments!!!",
-        ]
-        for store in stores:
-            messages.append(
-                f"Store #{store['storeNumber']} at " f"{store['fullAddress']}"
-            )
-        messages.append(f"➡ To register, go to {SCHEDULER_ENDPOINT}")
-        output = "\n\n".join(messages)
-        if send_notifications:
-            self.notifier.send_texts(
-                output, phone_numbers=[ph for ph, email in self.subscribers]
-            )
-            self.logger.info(
-                f"Sent texts to subscribers: {pformat(self.subscribers)}"
-            )
-        self.logger.info(output)
-
-    def get_stores(self, zip_code, radius):
+    def _get_stores(self, zip_code, radius):
         """
         Get list of RiteAid stores in zip code within radius
         """
